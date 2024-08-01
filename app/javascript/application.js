b4441c5cfbd195bbd873285885f3d780e2c0d31c
ShinJaehun <gunsmoke.shin@gmail.com>
2024-08-01 12:16:58 +0900

    first commit with bootstrap

---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+//import "bootstrap"
+import * as bootstrap from 'bootstrap'
+
+console.log("넌 뒤졌어~")
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+console.log(popoverList)
+
+//var myModal = document.getElementById('myModal')
+//var myInput = document.getElementById('myInput')
+//myModal.addEventListener('shown.bs.modal', function () {
+  //myInput.focus()
+//})
+
